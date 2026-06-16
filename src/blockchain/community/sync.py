05c57d8f8b5bad9db3991139e0221b950738a00f
Marina Mădăraș <m.madaras@student.tudelft.nl>
2026-06-16 17:07:08 +0200

    add print statement when we request unknown block

--- src/blockchain/community/sync.py
+++ src/blockchain/community/sync.py
@@ -52,6 +52,7 @@ def on_block_response(community, peer, payload):
         print(f"Received {block.block_hash.hex()[:16]} from {peer}")
         community.chain.add_block(block)
         if not community.chain.contains(block.header.prev_hash):
+            print(f"[Sync] Unknown parent {block.header.prev_hash.hex()[:16]} for {block.block_hash.hex()[:16]}, requesting from {peer}")
             community.ez_send(peer, RequestBlockByHash(block_hash=block.header.prev_hash))
     except Exception as exc:
         print(f"Invalid block from {peer}: {exc}")
